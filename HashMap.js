0c17b0e639d08953c28c0c6675ae0097ea2033f7
Coleman Grustas <grustas.c@northeastern.edu>
2025-11-08 12:56:41 -0500

    Add #hash(key)

--- HashMap.js
+++ HashMap.js
@@ -9,4 +9,20 @@ export class HashMap {
     this.#loadFactor = loadFactor;
     this.#capacity = capacity;
   }
+
+  /**
+   * Takes a key and produces a hash code
+   * @param {string} key - key to be hashed
+   * @returns {number} hash code
+   */
+  #hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
+
+    return hashCode;
+  }
 }
